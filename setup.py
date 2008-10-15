cf820acd892ce13c97379e763b86853090e59117
mbouchar <mbouchar@spectre.bioinfo.ulaval.ca>
2008-10-15 16:05:58 -0400

    debian package

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import sys, os
+
+if __name__ == "__main__":
+    setup(scripts    = ["bin/savemysqldb"],
+          data_files =[('/etc', ['conf/savemysqldb.conf']),
+                       ('/etc/cron.d', ['conf/savemysqldb.cron']),
+         )
