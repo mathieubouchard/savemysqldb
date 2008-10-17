f7d3428bffb0c3a34ffbeac48234fcb7a1bffa99
mbouchar <mbouchar@spectre.bioinfo.ulaval.ca>
2008-10-17 09:54:26 -0400

    install the cron file with a correct name

--- setup.py
+++ setup.py
@@ -4,7 +4,4 @@ from distutils.core import setup
 import sys, os
 
 if __name__ == "__main__":
-    setup(scripts    = ["bin/savemysqldb"],
-          data_files =[('/etc', ['conf/savemysqldb.conf']),
-                       ('/etc/cron.d', ['conf/savemysqldb.cron'])],
-         )
+    setup(scripts    = ["bin/savemysqldb"])
